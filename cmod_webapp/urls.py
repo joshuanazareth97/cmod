e1cea73497ad54f58cd7f21471dbbecf5912bdf8
Joshua <joshuanazareth97@gmail.com>
2019-01-21 10:11:40 +0530

    Added URLs for Auth and Homepage

--- cmod_webapp/urls.py
+++ cmod_webapp/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from comments import urls as comments_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
+    path('', include(comments_urls))
 ]
