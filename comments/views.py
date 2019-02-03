96a653003c6aed5ff4f85dcf107eb865496badb3
Joshua <joshuanazareth97@gmail.com>
2019-02-04 02:16:28 +0530

    Added empty form to create candidate view

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Candidate
+from .forms import CandidateForm
 # Create your views here.
 
 @login_required
@@ -20,4 +23,14 @@ def all_candidates(request):
 
     }
     print(context["candidates"])
-    return render(request, "all_candidates.html", context)
+    return render(request, "candidates/all_candidates.html", context)
+
+@login_required
+def create_candidate(request):
+    form = CandidateForm()
+    context = {
+        "form": form
+    }
+    html_form = render_to_string("candidates/includes/create_form.html", context, request)
+    print("form requested")
+    return JsonResponse ({"html_form":html_form})
