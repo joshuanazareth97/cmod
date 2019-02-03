fccacca35f47d9c0dd15b28b1cc0fc5c0672842c
Joshua <joshuanazareth97@gmail.com>
2019-02-03 23:05:17 +0530

    Added all candidates view

--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Candidate
 # Create your views here.
 
 @login_required
 def homepage(request):
-    return render(request, "homepage.html")
+    context = {
+        "user": request.user,
+
+    }
+    return render(request, "homepage.html", context)
+
+@login_required
+def all_candidates(request):
+    candidate = Candidate.objects.filter(creator=request.user)
+    context = {
+        "user": request.user,
+        "candidates": Candidate.objects.filter(creator=request.user)
+
+    }
+    print(context["candidates"])
+    return render(request, "all_candidates.html", context)
