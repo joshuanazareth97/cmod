082bb718910c8af6293bbf838fdf69401e7c39a8
Joshua <joshuanazareth97@gmail.com>
2019-02-03 23:17:11 +0530

    Added url for create_candidate view

--- comments/urls.py
+++ comments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import homepage, all_candidates
+from .views import homepage, all_candidates, create_candidate
 
 
 urlpatterns = [
     path("", homepage, name="home"),
-    path("candidates/all", all_candidates, name="all_candidates")
+    path("candidates/all", all_candidates, name="all_candidates"),
+    path("candidates/create", create_candidate, name="create_candidate"),
 ]
