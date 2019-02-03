eb2479da71bfe7ac4a2323fef8f0a2477ec85ae0
Joshua <joshuanazareth97@gmail.com>
2019-02-03 23:05:31 +0530

    Registered models with admin site

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Candidate, Comment, Reminder, Document
 # Register your models here.
+admin.site.register(Candidate)
+admin.site.register(Comment)
+admin.site.register(Document)
+admin.site.register(Reminder)
