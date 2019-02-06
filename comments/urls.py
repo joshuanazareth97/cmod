dc34fa051a5853e2c0d5a7224e8dfd386b8ce926
Joshua <joshuanazareth97@gmail.com>
2019-02-06 22:38:48 +0530

    added url to edit candidate

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import homepage, all_candidates, create_candidate
+from .views import homepage, all_candidates, create_candidate, edit_candidate
 
 
 urlpatterns = [
     path("", homepage, name="home"),
     path("candidates/all", all_candidates, name="all_candidates"),
     path("candidates/create", create_candidate, name="create_candidate"),
+    path("candidates/<cid>/edit", edit_candidate, name="edit_candidate")
 ]
