5db983696c255f44abb72f3024da2a0150605f93
Joshua <joshuanazareth97@gmail.com>
2019-02-07 17:04:54 +0530

    Overridden __init__method for form to change field if edit param passed

--- comments/forms.py
+++ comments/forms.py
@@ -5,3 +5,9 @@ class CandidateForm(forms.ModelForm):
     class Meta:
         model = Candidate
         fields = ('cid', 'name', 'designation')
+
+    def __init__(self, *args, **kwargs):
+        edit = kwargs.pop('edit', False)
+        super(CandidateForm, self).__init__(*args, **kwargs)
+        if edit:
+            self.fields.pop('cid')
