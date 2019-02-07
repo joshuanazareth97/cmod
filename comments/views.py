51f3a184dc8ac6e64564af5b245718bf0b10ba06
Joshua <joshuanazareth97@gmail.com>
2019-02-07 17:05:22 +0530

    Extracted create_candidate view functionality to generic save_candidate fn

--- comments/views.py
+++ comments/views.py
@@ -26,27 +26,36 @@ def all_candidates(request):
     }
     return render(request, "candidates/all_candidates.html", context)
 
-@login_required
-def create_candidate(request):
+def save_candidate(request, form, edit=False):
+    if edit:
+        template = "candidates/includes/edit_form.html"
+    else:
+        template = "candidates/includes/create_form.html"
     if request.is_ajax():
         data = {}
         if request.method == "POST":
-            form = CandidateForm(request.POST)
             if form.is_valid():
                 candidate = form.save(commit=False)
-                candidate.creator = request.user
+                if not edit:
+                    candidate.creator = request.user
                 candidate.save()
                 data["is_valid"] = True
                 candidates = Candidate.objects.filter(creator=request.user).order_by("name")
                 data["html_candidate_list"] = render_to_string("candidates/includes/candidate_list.html",{"candidates":candidates})
             else:
                 data["is_valid"] = False
-        else: #GET request
-            form = CandidateForm()
-        data["html_form"] = render_to_string("candidates/includes/create_form.html", {"form": form}, request)
+        data["html_form"] = render_to_string(template, {"form": form}, request)
         return JsonResponse(data)
     else:
         raise PermissionDenied("Cannot access this endpoint in this manner.")
+
+@login_required
+def create_candidate(request):
+    if request.method == "POST":
+        form = CandidateForm(request.POST)
+    else:
+        form = CandidateForm()
+    return save_candidate(request, form)
 
 @login_required
 def edit_candidate(request, cid):
