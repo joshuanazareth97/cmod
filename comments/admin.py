7a369e916ff38f882a4a482d4ac08a62a1f971d9
Joshua <joshuanazareth97@gmail.com>
2019-03-06 15:33:08 +0530

    Added option to view readonly fields in Comments model to admin.py

--- comments/admin.py
+++ comments/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Candidate, Comment, Reminder, Document
 # Register your models here.
 admin.site.register(Candidate)
-admin.site.register(Comment)
+# admin.site.register(Comment)
 admin.site.register(Document)
 admin.site.register(Reminder)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    readonly_fields=('created','modified','hash')
