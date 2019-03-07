fc27100e2306752f12e316a7c18c8225126d7efb
Joshua <joshuanazareth97@gmail.com>
2019-03-07 17:27:41 +0530

    fixed all_candidate_comments url name

--- comments/urls.py
+++ comments/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("candidates/create", create_candidate, name="create_candidate"),
     path("candidates/<cid>/edit", edit_candidate, name="edit_candidate"),
     path("candidates/<cid>/delete", delete_candidate, name="delete_candidate"),
-    path("candidates/<cid>/comments/all", all_candidate_comments, name="candidate_comments"),
+    path("candidates/<cid>/comments/all", all_candidate_comments, name="all_comments"),
     path("candidates/<cid>/comments/create", create_candidate_comment, name="create_comment"),
     path("comments/<hash_id>/edit", edit_candidate_comment, name="edit_comment")
 ]
