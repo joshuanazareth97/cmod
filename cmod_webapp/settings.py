9a77a6cd32e03720548e22fd67c23e304636741f
Joshua <joshuanazareth97@gmail.com>
2019-03-11 22:20:54 +0530

    Added static files relevant settings

--- cmod_webapp/settings.py
+++ cmod_webapp/settings.py
@@ -120,5 +120,9 @@ LOGOUT_REDIRECT_URL = "/login"
 LOGIN_URL = "/login"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,  'static')
+}
