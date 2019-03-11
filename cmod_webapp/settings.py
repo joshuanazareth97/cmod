b201e7b4441abbbe7a812e14054d31812c3d1ccc
Joshua <joshuanazareth97@gmail.com>
2019-03-11 23:07:59 +0530

    fixed staticfiles_dirs setting

--- cmod_webapp/settings.py
+++ cmod_webapp/settings.py
@@ -125,5 +125,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,  'static')
+    os.path.join(BASE_DIR,  'static'),
 )
