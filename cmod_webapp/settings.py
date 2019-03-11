b8d2076a7d24a6bf465dcd1c1c8e57d1b5115a22
Joshua <joshuanazareth97@gmail.com>
2019-03-11 23:51:06 +0530

    Added line to make directory

--- cmod_webapp/settings.py
+++ cmod_webapp/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,  'static'),
 )
+
+os.makedirs(STATIC_ROOT, exist_ok=True)
+os.makedirs(os.path.join(BASE_DIR,  'static'), exist_ok=True)
