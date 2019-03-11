2c981e775f9f22ddf98421d5acbc06e7249b654b
Joshua <joshuanazareth97@gmail.com>
2019-03-11 23:16:11 +0530

    Added allowed hosts load from environment variables in prod environment

--- cmod_webapp/settings.py
+++ cmod_webapp/settings.py
@@ -27,8 +27,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
-
+if DEBUG:
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda x: x.split(","))
 
 # Application definition
 
